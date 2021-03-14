abb5aa9cd088e5062cf1c0f8fb9ccc002fa6441a
oreze <kunas1@o2.pl>
2021-03-14 16:18:01 +0100

    Add some funny changes :-DDDDD

--- js/site.js
+++ js/site.js
@@ -100,5 +100,68 @@ const getImage = function () {
 }
 getImage();
 
+let counter = 0;
+const clickCounter = function () {
+    if (counter < 200)
+        counter += 1;
+    else if (counter == 200) {
+        initKotyraEgg();
+    }
+}
+
+const initKotyraEgg = function () {
+    addBlinkingBackground()
+    playAudio();
+    addRandomImage("images/randomKotyra.jpg");
+    addRandomImage("images/backgrounds/chinaFlag.jpg");
+    setBackgroundFlagForButtons();
+}
+
+const setBackgroundFlagForButtons = function () {
+    document.querySelectorAll(".but").forEach(el => {
+        el.style.backgroundColor = "transparent";
+        el.style.backgroundImage = "url('images/backgrounds/chinaFlag.jpg')";
+        el.style.backgroundSize = "cover";
+    })
+}
+
+const playAudio = function () {
+    const audio = new Audio("audio/xcq.mp3");
+    audio.volume = 1;
+    audio.loop = true;
+    audio.load();
+    audio.play();
+    counter = 101;
+}
+
+const addRandomImage = async function (imagePath) {
+    const img = document.createElement("img");
+    img.id = "randomKotyraFace";
+    img.src = imagePath;
+    img.style.position = "absolute";
+    img.style.zIndex = 100;
+    document.body.append(img);
+    img.style.right = Math.floor(Math.random() * 80) + "%";
+    img.style.bottom = Math.floor(Math.random() * 80) + "%";
+    img.style.width = Math.floor(Math.random() * 50) + "%";
+
+    setTimeout(() => {
+        document.body.removeChild(img);
+        addRandomImage(imagePath);
+    }, 100);
+    console.log("XD2");
+}
+
+const addBlinkingBackground = function () {
+    const background = document.createElement("div");
+    background.classList.add("blinking-background");
+    document.body.style.backgroundImage = "url('images/backgrounds/borzecki.jpg')"
+    background.style.animation = ".2s ease epilepsy infinite";
+    background.style.animationPlayState = "running";
+    document.body.appendChild(background);
+}
+
+
+document.addEventListener("click", clickCounter);
 document.getElementById("btn-random-meme").addEventListener("click", getImage);
 
