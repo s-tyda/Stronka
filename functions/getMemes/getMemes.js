c414f021d22dfcfedb69b58cf16e627577a608d9
s-tyda <styda@huhha.co>
2021-03-21 19:44:58 +0100

    testing new method

--- functions/getMemes/getMemes.js
+++ functions/getMemes/getMemes.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
+const readline = require('readline');
+var files = []
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./assets/memes.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', (line) => {
+    console.log(line);
+    files.push(line)
+});
 // var files = fs.readdirSync('./images/memes/');
 // var files = fs.readdirSync('/var/runtime/build/Release');
-const files = fs.readFileSync(require.resolve('./assets/memes.txt'));
+// const files = fs.readFileSync(require.resolve('./assets/memes.txt'));
 // const memes = files.split(/\n/);
 // var directories = []
 // for (var i = 0; i < files.length; i++){
