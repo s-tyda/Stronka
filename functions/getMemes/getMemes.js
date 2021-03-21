2df6e1c7c2a6dd1b3ad0dee4aeac78d8510e1876
s-tyda <styda@huhha.co>
2021-03-21 21:15:27 +0100

    removed unnecessary code

--- functions/getMemes/getMemes.js
+++ functions/getMemes/getMemes.js
@@ -1,32 +1,5 @@
 const fs = require('fs');
-const readline = require('readline');
 var files = fs.readFileSync(require.resolve('./assets/memes.txt')).toString().split("\n");
-
-// const rl = readline.createInterface({
-//     input: fs.createReadStream(require.resolve('./assets/memes.txt')),
-//     output: process.stdout,
-//     terminal: false
-// });
-//
-// rl.on('line', (line) => {
-//     console.log(line);
-//     files.push(line);
-// });
-// var files = fs.readdirSync('./images/memes/');
-// var files = fs.readdirSync('/var/runtime/build/Release');
-// const files = fs.readFileSync(require.resolve('./assets/memes.txt'));
-// const memes = files.split(/\n/);
-// var directories = []
-// for (var i = 0; i < files.length; i++){
-//     try{
-//         directories.push({
-//             name: files[i],
-//             content: fs.readdirSync('/var/' + files[i])
-//         });
-//     } catch (err){
-//         continue;
-//     }
-// }
 
 exports.handler = function (event, context, callback){
     callback(null, {
