f9061aa0de24dc5c15e85e1e74df84740f86c161
s-tyda <styda@huhha.co>
2021-03-21 18:39:22 +0100

    fix

--- functions/getMemes.js
+++ functions/getMemes.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 // var files = fs.readdirSync('./images/memes/');
-var files = fs.readdirSync('../' + __dirname);
+var files = fs.readdirSync('..' + __dirname);
 
 exports.handler = function (event, context, callback){
     callback(null, {
